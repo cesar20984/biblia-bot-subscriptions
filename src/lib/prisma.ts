dbf84e898c8dedd9331604d95886c2507dd30026
Cesar <cesar@bibliabendita.com>
2026-03-01 19:43:32 -0300

    Build fix: standard PrismaClient initialization

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -5,8 +5,7 @@ const globalForPrisma = global as unknown as { prisma: PrismaClient };
 export const prisma =
     globalForPrisma.prisma ||
     new PrismaClient({
-        datasourceUrl: process.env.DATABASE_URL,
         log: ['query'],
-    } as any);
+    });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
